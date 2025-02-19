1885ed3716a1392b3ff8269aacd06cffe9a8680c
Mayank <mayanktiwari1212.mt@gmail.com>
2025-02-19 09:53:54 +0530

    feat: Removed linkga

--- components/GuidelinesPageContent.tsx
+++ components/GuidelinesPageContent.tsx
@@ -34,16 +34,7 @@ export default function GuidelinesPage() {
             <ul className="list-disc">
               <li>
                 All papers must be submitted through the EasyChair system using
-                the following{" "}
-                <a
-                  href="https://easychair.org/conferences/?conf=nccc2025"
-                  target="_blank"
-                  className="font-bold underline text-blue-500"
-                >
-                  {" "}
-                  link
-                </a>
-                .
+                the following link .
               </li>
             </ul>
           </p>
